5c2620c983234a0774ac7aa8fcb1b290b81e35af
Luciano Gallo <102675296+luchogallo92@users.noreply.github.com>
2024-06-30 18:39:08 -0300

    fixes

--- assets/JS/hero.js
+++ assets/JS/hero.js
@@ -1,18 +1,22 @@
 const hero_images = [
     {
-        id: 1,
-        img: "./assets/hero_images/BAN1-01.png"
-    },
-    {
         id: 2,
-        img: "./assets/hero_images/motor.png"
+        img: "./assets/hero_images/BAN2-02.png"
     },
     {
         id: 3,
-        img: "./assets/hero_images/BAN1-01.png"
+        img: "./assets/hero_images/BAN3-03.png"
     },
     {
         id: 4,
-        img: "./assets/hero_images/motor.png"
+        img: "./assets/hero_images/BAN4-04.png"
+    },
+    {
+        id: 5,
+        img: "./assets/hero_images/BAN5-05.png"
+    },
+    {
+        id: 1,
+        img: "./assets/hero_images/BAN1-01.png"
     },
 ]
